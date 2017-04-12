f6ff133cb7f13a437c9f7443894a0415a9b23897
Adrien SAUNIER <asaunier@octo.com>
2017-04-12 15:04:40 +0200

    Few improvements

--- test/first.test.js
+++ test/first.test.js
@@ -1,16 +1,16 @@
-var assert = require('assert');
-var expect = require('chai').expect;
-var should = require('chai').should();
-var appFirst = require('../app/first');
+const assert = require('assert');
+const expect = require('chai').expect;
+const should = require('chai').should();
+const appFirst = require('../app/first');
 
-describe('First', function () {
-  describe('#isFalse', function () {
-    it('should return false', function () {
+describe('First', () => {
+  describe('#isFalse', () => {
+    it('should return false', () => {
       // Given
-      var expected = false;
+      const expected = false;
 
       // When
-      var result = appFirst.isFalse(true);
+      const result = appFirst.isFalse(true);
 
       // Then
       assert.equal(result, expected);
